a47175f36d60906b994bd9b706189f0e4fa428a9
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2023-05-18 09:05:09 +0200

    Major Updates

--- init.el
+++ init.el
@@ -1,395 +1,161 @@
-;; Answer with y/n instead of yes/no
-(defalias 'yes-or-no-p 'y-or-n-p)
- 
-;; The default is 800 kilobytes.  Measured in bytes.
-(setq gc-cons-threshold (* 50 1000 1000))
-
-;; Initialize package sources
+;; Initialisiert Paketverwaltung
 (require 'package)
-(setq package-archives '(("melpa" . "https://melpa.org/packages/")
-                         ("org" . "https://orgmode.org/elpa/")
-                         ("elpa" . "https://elpa.gnu.org/packages/")))
+
+;; Einstellungen
+(setq user-full-name "Raphaele Salvatore Licciardo, B.Sc."
+      user-mail-address "raphaele.salvatore@outlook.de"
+      display-line-numbers-type 'relative ;; Relative Zeilen Nummber
+      global-display-line-numbers-mode t  ;; Zeilen Nummern
+      mac-command-modifier 'meta ;; Deutsche MacOS Tastatur
+      mac-option-modifier 'none
+      default-input-method "MacOSX"
+      make-backup-files nil ;; Verhindert das Erstellen von Backup-Dateien
+      auto-save-default nil ;; Verhindert das Erstellen von automatisch gespeicherten Dateien
+      package-enable-at-startup nil ;; Paketverwaltung
+      ring-bell-function 'ignore ;; Keine Klingel mehr
+)
+
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+
 (package-initialize)
+
+;; Aktualisiert die Paketdatenbank, falls sie noch nicht vorhanden ist
 (unless package-archive-contents
   (package-refresh-contents))
 
-;; Initialize use-package on non-Linux platforms
+;; Installiert use-package, falls es noch nicht vorhanden ist
 (unless (package-installed-p 'use-package)
   (package-install 'use-package))
+
+;; Benutzen Sie Paketverwaltung für alle weiteren Pakete
 (require 'use-package)
-(setq use-package-always-ensure t)
+(setq use-package-always-ensure t) ;; Automatisches Herunterladen und Installieren fehlender Pakete
 
-;; Auto Update
-(use-package auto-package-update
-  :custom
-  (auto-package-update-interval 7)
-  (auto-package-update-prompt-before-update t)
-  (auto-package-update-hide-results t)
-  :config
-  (auto-package-update-maybe)
-  (auto-package-update-at-time "09:00"))
+;; File Explorer
+(if (executable-find "gls")
+    (setq insert-directory-program "gls"))
+(use-package dired
+  :ensure nil
+  :commands (dired dired-jump)
+  :custom ((dired-listing-switches "-agho --group-directories-first")))
 
-;; Help keeping ~/.emacs.d clean
-(use-package no-littering)
-(setq auto-save-file-name-transforms
-      `((".*" ,(no-littering-expand-var-file-name "auto-save/") t)))
-
-;; Startup Messages
-(setq inhibit-startup-message t)
-
-;; Change UI
-;;(scroll-bar-mode -1)        ; Disable visible scrollbar
-(tool-bar-mode -1)            ; Disable the toolbar
-(tooltip-mode -1)             ; Disable tooltips
-;;(set-fringe-mode 10)        ; Give some breathing room
-(menu-bar-mode -1)            ; Disable the menu bar
-
-;; Set up the visible bell
-(setq visible-bell t)
-
-;; Line Numbers
-(column-number-mode)
-;;(global-display-line-numbers-mode 1)
-;;(setq display-line-numbers-type 'relative)
-(global-display-line-numbers-mode t)
-
-;; Disable line numbers for some modes
-(dolist (mode '(org-mode-hook
-                term-mode-hook
-                shell-mode-hook
-                treemacs-mode-hook
-                eshell-mode-hook))
-  (add-hook mode (lambda () (display-line-numbers-mode 0))))
-
-;; Make ESC quit prompts
-(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
-
-;; More convenient key definitions in emacs 
-(use-package general
-  :after evil
-  :config
-  (general-create-definer efs/leader-keys
-    :keymaps '(normal insert visual emacs)
-    :prefix "SPC"
-    :global-prefix "C-SPC")
-
-  (efs/leader-keys
-    "t"  '(:ignore t :which-key "toggles")
-    "tt" '(counsel-load-theme :which-key "choose theme")
-    "fde" '(lambda () (interactive) (find-file (expand-file-name "~/.emacs.d/Emacs.org")))))
-
-;; VIM MOVEMENT!!!
+;; Lädt und konfiguriert evil
 (use-package evil
   :init
   (setq evil-want-integration t)
   (setq evil-want-keybinding nil)
-  (setq evil-want-C-u-scroll t)
-  (setq evil-want-C-i-jump nil)
   :config
   (evil-mode 1)
-  (define-key evil-insert-state-map (kbd "C-s") 'evil-normal-state)
-  (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
+  (add-to-list 'evil-emacs-state-modes 'dired-mode)
+  )
 
-  ;; Use visual line motions even outside of visual-line-mode buffers
-  (evil-global-set-key 'motion "j" 'evil-next-visual-line)
-  (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
+;; Git Client verwenden
+(use-package magit
+  :ensure t)
 
-  (evil-set-initial-state 'messages-buffer-mode 'normal)
-  (evil-set-initial-state 'dashboard-mode 'normal))
-(use-package evil-collection
-  :after evil
-  :config
-  (evil-collection-init))
-
-;; Display Keymaps
-(use-package which-key
-  :defer 0
-  :diminish which-key-mode
-  :config
-  (which-key-mode)
-  (setq which-key-idle-delay 1))
-
-;; Auto Completion Engine
-(use-package ivy
-  :diminish
-  :bind (("C-s" . swiper)
-         :map ivy-minibuffer-map
-         ("TAB" . ivy-alt-done)
-         ("C-l" . ivy-alt-done)
-         ("C-j" . ivy-next-line)
-         ("C-k" . ivy-previous-line)
-         :map ivy-switch-buffer-map
-         ("C-k" . ivy-previous-line)
-         ("C-l" . ivy-done)
-         ("C-d" . ivy-switch-buffer-kill)
-         :map ivy-reverse-i-search-map
-         ("C-k" . ivy-previous-line)
-         ("C-d" . ivy-reverse-i-search-kill))
-  :config
-  (ivy-mode 1))
-(use-package ivy-rich
-  :after ivy
+;; Automatischen schließen von Klammern und co.
+(use-package smartparens
+  :ensure t
   :init
-  (ivy-rich-mode 1))
+  (smartparens-global-mode))
 
-;; a collection of Ivy-enhanced versions of common Emacs commands.
-(use-package counsel
-  :bind (("C-M-j" . 'counsel-switch-buffer)
-         :map minibuffer-local-map
-         ("C-r" . 'counsel-minibuffer-history))
-  :custom
-  (counsel-linux-app-format-function #'counsel-linux-app-format-function-name-only)
-  :config
-  (counsel-mode 1))
+;; Themes
+(use-package doom-themes
+  :ensure t
+  :preface (defvar region-fg nil) ; this prevents a weird bug with doom themes
+  ;;:init (load-theme 'doom-one t)
+  )
 
-;; Fast and intuitive frequency-and-recency-based sorting and filtering for Ivy
-(use-package ivy-prescient
-  :after counsel
-  :custom
-  (ivy-prescient-enable-filtering nil)
-  :config
-  ;; Uncomment the following line to have sorting remembered across sessions!
-  ;(prescient-persist-mode 1)
-  (ivy-prescient-mode 1))
+;; yes / no Fragen abkürzen 
+(fset 'yes-or-no-p 'y-or-n-p)
 
-;; Manual Pages for Emacs
-(use-package helpful
-  :commands (helpful-callable helpful-variable helpful-command helpful-key)
-  :custom
-  (counsel-describe-function-function #'helpful-callable)
-  (counsel-describe-variable-function #'helpful-variable)
-  :bind
-  ([remap describe-function] . counsel-describe-function)
-  ([remap describe-command] . helpful-command)
-  ([remap describe-variable] . counsel-describe-variable)
-  ([remap describe-key] . helpful-key))
-
-;; MacOS Dired Stuff
-(when (string= system-type "darwin")
-  (setq dired-use-ls-dired t
-        insert-directory-program "/usr/local/bin/gls"
-        dired-listing-switches "-aBhl --group-directories-first"))
-
-;; Make bindings that stick around
-(use-package hydra
-  :defer t)
-(defhydra hydra-dired (:exit t)
-  "dired"
-  ("w" (dired "~/Downloads") "Downloads")
-  ("d" (dired "~/Documents") "Documents"))
-(global-set-key (kbd "C-c C-d") 'hydra-dired/body)
-(defhydra hydra-text-scale (:timeout 4)
-  "scale text"
-  ("j" text-scale-increase "in")
-  ("k" text-scale-decrease "out")
-  ("f" nil "finished" :exit t))
-(efs/leader-keys
-  "ts" '(hydra-text-scale/body :which-key "scale text"))
-
-;; LSP
-;;(defun efs/lsp-mode-setup ()
-;;  (setq lsp-headerline-breadcrumb-segments '(path-up-to-project file symbols))
-;;  (lsp-headerline-breadcrumb-mode))
 ;; LSP Mode
 (use-package lsp-mode
-  :commands (lsp lsp-deferred)
-  ;;:hook (lsp-mode . efs/lsp-mode-setup)
-  :init
-  (setq lsp-keymap-prefix "C-c l")  ;; Or 'C-l', 's-l'
-  :config
-  (lsp-enable-which-key-integration t))
-;; LSP UI
-(use-package lsp-ui
-  :hook (lsp-mode . lsp-ui-mode)
-  :custom
-  (lsp-ui-doc-position 'bottom))
-;; LSP Auto Completion
-(use-package lsp-ivy
-  :after lsp)
-
-;; C LSP 
-(add-hook 'c-mode-hook 'lsp)
-
-;; C++ LSP 
-(add-hook 'c++-mode-hook 'lsp)
-
-;; Python LSP 
-(use-package python-mode
   :ensure t
-  :hook (python-mode . lsp-deferred)
-  :custom
-  (dap-python-debugger 'debugpy)
-  :config
-  (require 'dap-python))
-(setq lsp-pyls-server-command "/opt/homebrew/Caskroom/miniforge/base/bin/pylsp")
-(use-package pyvenv
-  :after python-mode
-  :config
-  (pyvenv-mode 1))
+  :hook ((python-mode c-mode c++-mode) . lsp)
+  :commands lsp)
 
-;; Text Completion
+(use-package lsp-ui
+  :ensure t
+  :commands lsp-ui-mode)
+
+(use-package lsp-python-ms
+  :ensure t
+  :init (setq lsp-python-ms-auto-install-server t)
+  :hook (python-mode . (lambda ()
+                          (require 'lsp-python-ms)
+                          (lsp))))  ; or lsp-deferred
+
+(use-package ccls
+  :ensure t
+  :hook ((c-mode c++-mode) . (lambda () (require 'ccls) (lsp))))  ; or lsp-deferred
+
 (use-package company
-  :after lsp-mode
-  :hook (lsp-mode . company-mode)
-  :bind (:map company-active-map
-         ("<tab>" . company-complete-selection))
-        (:map lsp-mode-map
-         ("<tab>" . company-indent-or-complete-common))
-  :custom
-  (company-minimum-prefix-length 1)
-  (company-idle-delay 0.0))
-(use-package company-box
-  :hook (company-mode . company-box-mode))
+  :ensure t
+  :init (add-hook 'after-init-hook 'global-company-mode))
 
-;; project interaction
-(use-package projectile
-  :diminish projectile-mode
-  :config (projectile-mode)
-  :custom ((projectile-completion-system 'ivy))
-  :bind-keymap
-  ("C-c p" . projectile-command-map)
-  :init
-  ;; NOTE: Set this to the folder where you keep your Git repos!
-  (when (file-directory-p "~/Developer")
-    (setq projectile-project-search-path '("~/Developer")))
-  (setq projectile-switch-project-action #'projectile-dired))
+;; Shortcuts
+(defun increase-font-size ()
+  (interactive)
+  (text-scale-increase 1))
 
-;; Ivy UI for Projects 
-(use-package counsel-projectile
-  :after projectile
-  :config (counsel-projectile-mode))
+(defun decrease-font-size ()
+  (interactive)
+  (text-scale-decrease 1))
 
-;; Git Client
-(use-package magit
-  :commands magit-status
-  :custom
-  (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1))
-
-;; Terminal
-(use-package term
-  :commands term
+(use-package which-key
+  :ensure t
   :config
-  (setq explicit-shell-file-name "zsh") ;; Change this to zsh, etc
-  (setq term-prompt-regexp "^[^#$%>\n]*[#$%>] *"))
+  (which-key-mode)
+  (which-key-add-key-based-replacements "SPC w" "Window")
+  (which-key-add-key-based-replacements "SPC b" "Buffer")
+  (which-key-add-key-based-replacements "SPC w r" "Enlarge horiz. split")
+  (which-key-add-key-based-replacements "SPC w l" "Shrink horiz. split")
+  (which-key-add-key-based-replacements "SPC w u" "Enlarge vert. split")
+  (which-key-add-key-based-replacements "SPC w d" "Shrink vert horizontally"))
 
-;; Terminal Colors
-(use-package eterm-256color
-  :hook (term-mode . eterm-256color-mode))
-
-;; Emacs Shell
-(defun efs/configure-eshell ()
-  (add-hook 'eshell-pre-command-hook 'eshell-save-some-history)
-  (add-to-list 'eshell-output-filter-functions 'eshell-truncate-buffer)
-  (evil-define-key '(normal insert visual) eshell-mode-map (kbd "C-r") 'counsel-esh-history)
-  (evil-define-key '(normal insert visual) eshell-mode-map (kbd "<home>") 'eshell-bol)
-  (evil-normalize-keymaps)
-  (setq eshell-history-size         10000
-        eshell-buffer-maximum-lines 10000
-        eshell-hist-ignoredups t
-        eshell-scroll-to-bottom-on-input t))
-(use-package eshell-git-prompt
-  :after eshell)
-(use-package eshell
-  :hook (eshell-first-time-mode . efs/configure-eshell)
+(use-package general
+  :ensure t
   :config
-  (with-eval-after-load 'esh-opt
-    (setq eshell-destroy-buffer-when-process-dies t)
-    (setq eshell-visual-commands '("htop" "zsh" "vim")))
-  (eshell-git-prompt-use-theme 'powerline))
+  (general-create-definer my-leader-def
+    :states '(normal visual)
+    :prefix "SPC"
+    :global-prefix "C-SPC")
+  
+  (my-leader-def
+    "." 'dired
+    "b i" 'buffer-menu
+    "b b" 'switch-to-buffer
+    "b p" 'previous-buffer
+    "b n" 'next-buffer
+    "b k" 'kill-this-buffer
+    "b s" 'save-buffer
+    "w v" 'split-window-right
+    "w s" 'split-window-below
+    "w w" 'other-window
+    "w q" 'delete-window
+    "w +" 'enlarge-window
+    "w -" 'shrink-window
+    "w r" 'enlarge-window-horizontally
+    "w l" 'shrink-window-horizontally
+    "w u" 'enlarge-window-horizontally
+    "w d" 'shrink-window-horizontally
+))
 
-;; File Explorer
-(use-package dired
-  :ensure nil
-  :commands (dired dired-jump)
-  :bind (("C-x C-j" . dired-jump))
-  :custom ((dired-listing-switches "-agho --group-directories-first"))
-  :config
-  (evil-collection-define-key 'normal 'dired-mode-map
-    "h" 'dired-single-up-directory
-    "l" 'dired-single-buffer))
-(use-package dired-single
-  :commands (dired dired-jump))
-(use-package dired-open
-  :commands (dired dired-jump)
-  :config
-  (setq dired-open-extensions '(("png" . "feh") ("mkv" . "mpv"))))
+(global-set-key (kbd "M-+") 'increase-font-size)
+(global-set-key (kbd "M--") 'decrease-font-size)
 
-;; Own Keymaps 
-(use-package dired-hide-dotfiles
-  :hook (dired-mode . dired-hide-dotfiles-mode)
-  :config
-  (evil-collection-define-key 'normal 'dired-mode-map
-    "H" 'dired-hide-dotfiles-mode))
-(defun add-to-map(keys func)
-  (define-key evil-normal-state-map (kbd keys) func)
-  (define-key evil-motion-state-map (kbd keys) func))
-(add-to-map "<SPC>" nil)
-(add-to-map "<SPC> f" 'find-file)
-(add-to-map "<SPC> S" 'save-buffer)
-(add-to-map "<SPC> s h" 'split-window-below)
-(add-to-map "<SPC> s s" 'split-window-right)
-(add-to-map "<SPC> s c" 'delete-window)
-(add-to-map "<SPC> r" 'compile)
-(add-to-map "<SPC> d" 'dired)
-(add-to-map "<SPC> g" 'magit)
-(add-to-map "<SPC> n" 'next-buffer)
-(add-to-map "<SPC> p" 'previous-buffer)
-(add-to-map "<SPC> b" 'buffer-menu)
-(add-to-map "<SPC> k" 'kill-current-buffer)
-;;(add-to-map "C->" 'text-scale-decrease)
-;;(add-to-map "C-<" 'text-scale-increase)
-(add-to-map "C-j" 'windmove-down)
-(add-to-map "C-k" 'windmove-up)
-(add-to-map "C-h" 'windmove-left)
-(add-to-map "C-l" 'windmove-right)
-
-;; MacOS Keyboard
-(setq default-input-method "MacOSX")
-(setq mac-command-modifier 'meta
-      mac-option-modifier nil
-      mac-allow-anti-aliasing t
-      mac-command-key-is-meta t)
-;; Umlaute
-(set-keyboard-coding-system 'iso-latin-1)
-;; alt is meta
-(set-keyboard-coding-system 'iso-latin-1)
-;; keymaps
-(if (eq system-type 'darwin)
-    (if (eq window-system 'mac)
-	(progn
-	  (setq mac-keyboard-text-encoding kTextEncodingISOLatin1)
-	  (latin1-display 'latin-9))
-      (if (eq window-system 'nil)
-	  (progn
-	    (global-set-key "\M-l" '(lambda () (interactive) (insert "@")))
-	    (global-set-key "\M-5" '(lambda () (interactive) (insert "[")))
-	    (global-set-key "\M-6" '(lambda () (interactive) (insert "]")))
-	    (global-set-key "\M-7" '(lambda () (interactive) (insert "|")))
-	    (global-set-key "\M-/" '(lambda () (interactive) (insert "\\")))
-	    (global-set-key "\M-8" '(lambda () (interactive) (insert "{")))
-	    (global-set-key "\M-9" '(lambda () (interactive) (insert "}")))
-	    (global-set-key "\M-n" '(lambda () (interactive) (insert "~")))
-	    ))))
-;; kj Escape key
-(setq key-chord-two-keys-delay 0.5)
-(key-chord-define evil-insert-state-map "kj" 'evil-normal-state)
-(key-chord-mode 1)
-
-;; Make gc pauses faster by decreasing the threshold.
-(setq gc-cons-threshold (* 2 1000 1000))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("7a424478cb77a96af2c0f50cfb4e2a88647b3ccca225f8c650ed45b7f50d9525" "be84a2e5c70f991051d4aaf0f049fa11c172e5d784727e0b525565bb1533ec78" "a138ec18a6b926ea9d66e61aac28f5ce99739cf38566876dc31e29ec8757f6e2" "c865644bfc16c7a43e847828139b74d1117a6077a845d16e71da38c8413a5aaa" "6945dadc749ac5cbd47012cad836f92aea9ebec9f504d32fe89a956260773ca4" "adaf421037f4ae6725aa9f5654a2ed49e2cd2765f71e19a7d26a454491b486eb" "56044c5a9cc45b6ec45c0eb28df100d3f0a576f18eef33ff8ff5d32bac2d9700" "5f128efd37c6a87cd4ad8e8b7f2afaba425425524a68133ac0efd87291d05874" "aec7b55f2a13307a55517fdf08438863d694550565dee23181d2ebd973ebd6b8" "afa47084cb0beb684281f480aa84dab7c9170b084423c7f87ba755b15f6776ef" "30dc9873c16a0efb187bb3f8687c16aae46b86ddc34881b7cae5273e56b97580" "bfc0b9c3de0382e452a878a1fb4726e1302bf9da20e69d6ec1cd1d5d82f61e3d" "dde643b0efb339c0de5645a2bc2e8b4176976d5298065b8e6ca45bc4ddf188b7" "02f57ef0a20b7f61adce51445b68b2a7e832648ce2e7efb19d217b6454c1b644" default))
  '(package-selected-packages
-   '(key-chord json-mode flycheck dired-hide-dotfiles dired-open dired-single eshell-git-prompt vterm eterm-256color rainbow-delimiters evil-nerd-commenter forge magit counsel-projectile projectile company-box company pyvenv python-mode lsp-ivy lsp-ui lsp-mode visual-fill-column which-key use-package no-littering ivy-rich ivy-prescient hydra helpful general evil-collection counsel auto-package-update))
- '(warning-suppress-types '((emacs))))
+   '(doom-themes smartparens magit use-package lsp-ui lsp-python-ms lsp-java lsp-ivy helm-lsp evil ccls)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+(put 'dired-find-alternate-file 'disabled nil)
