908f6731695f5f35a27a15e71a42d245983f628f
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2024-05-30 12:54:27 +0200

    fix: Major UI Changes

--- init.el
+++ init.el
@@ -45,30 +45,23 @@
 
 ;; No Startup Message
 (setq inhibit-startup-message t)
-(setq initial-scratch-message "\n\n\n")
+(setq initial-scratch-message "")
 
 ;; Cleanup the UI
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
 (tooltip-mode -1)
-(set-fringe-mode -1)
 (menu-bar-mode -1)
-(spacious-padding-mode 1)
 
 ;; Theme
-(load-theme 'modus-operandi-tinted 1)
-;; (load-theme 'gruber-darker 1)
+;; (load-theme 'modus-operandi-tinted 1)
+(load-theme 'gruber-darker 1)
 ;; (load-theme 'doom-one 1)
-
-;; Modeline
-(column-number-mode 1)
+;; (load-theme 'zenburn 1)
 
 (use-package mood-line
   :config
   (mood-line-mode 1)
-  ;; Use pretty Fira Code-compatible glyphs
-  :custom
-  (mood-line-glyph-alist mood-line-glyphs-fira-code)
 )
 
 ;; Font
@@ -79,10 +72,6 @@
    ((eq system-type 'gnu/linux) "Iosevka-12")))
 
 (add-to-list 'default-frame-alist `(font . ,(get-default-font)))
-
-;; UI Tweaks
-(setq left-fringe-width 0)
-(setq right-fringe-width 0)
 
 ;; Ido Mode for Files
 (ido-mode 1)
@@ -140,23 +129,6 @@
 ;; Smooth Scroll - Horizontal
 (setq hscroll-step 1)
 (setq hscroll-margin 1)
-
-;; Pixel Scroll Precision Mode
-(pixel-scroll-precision-mode 1)
-
-;; Prettify Symbols
-(global-prettify-symbols-mode 1)
-(defun add-pretty-lambda ()
-  "Make some word or string show as pretty Unicode symbols.  See https://unicodelookup.com for more."
-  (setq prettify-symbols-alist
-        '(("lambda" . 955)
-          ("delta" . 120517)
-          ("epsilon" . 120518)
-          ("->" . 8594)
-          ("<=" . 8804)
-          (">=" . 8805))))
-(add-hook 'prog-mode-hook 'add-pretty-lambda)
-(add-hook 'org-mode-hook 'add-pretty-lambda)
 
 ;; Dired
 (use-package dired
@@ -242,11 +214,6 @@
      (mark " "
            (name 16 -1)
            " " filename))))
-
-(use-package super-save
-  :ensure t
-  :config
-  (super-save-mode +1))
 
 ;; ---------------------------------------------------------------------------------
 ;; -------- Shortcuts --------------------------------------------------------------
@@ -389,13 +356,14 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("c7a926ad0e1ca4272c90fce2e1ffa7760494083356f6bb6d72481b879afce1f2" "0f76f9e0af168197f4798aba5c5ef18e07c926f4e7676b95f2a13771355ce850" "e27c9668d7eddf75373fa6b07475ae2d6892185f07ebed037eedf783318761d7" "88f7ee5594021c60a4a6a1c275614103de8c1435d6d08cc58882f920e0cec65e" "7613ef56a3aebbec29618a689e47876a72023bbd1b8393efc51c38f5ed3f33d1" "d77d6ba33442dd3121b44e20af28f1fae8eeda413b2c3d3b9f1315fbda021992" "e13beeb34b932f309fb2c360a04a460821ca99fe58f69e65557d6c1b10ba18c7" "9f297216c88ca3f47e5f10f8bd884ab24ac5bc9d884f0f23589b0a46a608fe14" "285d1bf306091644fb49993341e0ad8bafe57130d9981b680c1dbd974475c5c7" "4c56af497ddf0e30f65a7232a8ee21b3d62a8c332c6b268c81e9ea99b11da0d3" "4b026ac68a1vaa4d1a91879b64f54c2490b4ecad8b64de5b1865bca0addd053d9" "21e3d55141186651571241c2ba3c665979d1e886f53b2e52411e9e96659132d4" default))
+   '("f079ef5189f9738cf5a2b4507bcaf83138ad22d9c9e32a537d61c9aae25502ef" "c7a926ad0e1ca4272c90fce2e1ffa7760494083356f6bb6d72481b879afce1f2" "0f76f9e0af168197f4798aba5c5ef18e07c926f4e7676b95f2a13771355ce850" "e27c9668d7eddf75373fa6b07475ae2d6892185f07ebed037eedf783318761d7" "88f7ee5594021c60a4a6a1c275614103de8c1435d6d08cc58882f920e0cec65e" "7613ef56a3aebbec29618a689e47876a72023bbd1b8393efc51c38f5ed3f33d1" "d77d6ba33442dd3121b44e20af28f1fae8eeda413b2c3d3b9f1315fbda021992" "e13beeb34b932f309fb2c360a04a460821ca99fe58f69e65557d6c1b10ba18c7" "9f297216c88ca3f47e5f10f8bd884ab24ac5bc9d884f0f23589b0a46a608fe14" "285d1bf306091644fb49993341e0ad8bafe57130d9981b680c1dbd974475c5c7" "4c56af497ddf0e30f65a7232a8ee21b3d62a8c332c6b268c81e9ea99b11da0d3" "4b026ac68a1vaa4d1a91879b64f54c2490b4ecad8b64de5b1865bca0addd053d9" "21e3d55141186651571241c2ba3c665979d1e886f53b2e52411e9e96659132d4" default))
  '(package-selected-packages
-   '(format-all magit gruber-darker-theme mood-line hide-mode-line all-the-icons-nerd-fonts all-the-icons doom-modeline doom-themes vundo ## avy company flycheck helm-lsp helm-xref hydra lsp-mode projectile yasnippet)))
+v   '(zenburn-theme format-all magit gruber-darker-theme mood-line hide-mode-line all-the-icons-nerd-fonts all-the-icons doom-modeline doom-themes vundo ## avy company flycheck helm-lsp helm-xref hydra lsp-mode projectile yasnippet)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+
 )
 
