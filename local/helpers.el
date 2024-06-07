7af08ef9b14cc07b89c64107f6d10bf13bb6b7d4
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2024-06-07 07:24:04 +0200

    Even More helpers oO

---
+++ local/helpers.el
@@ -0,0 +1,13 @@
+;; Default Font
+(defun get-default-font ()
+  (cond
+   ((eq system-type 'windows-nt) "Iosevka-12")
+   ((eq system-type 'darwin) "Iosevka-14")
+   ((eq system-type 'gnu/linux) "Iosevka-12")))
+
+;; Deutsche Mac Tastatur
+(if (eq system-type 'darwin)
+    (setq mac-command-modifier 'meta
+	  mac-option-modifier 'none
+	  default-input-method "MacOSX"))
+
