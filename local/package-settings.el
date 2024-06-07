90a69a70424174c48710a3f002bd54aeef8209b5
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2024-06-07 07:25:53 +0200

    The Rest of the Helpers

--- local/package-settings.el
+++ local/package-settings.el
@@ -42,3 +42,8 @@
      (mark " "
            (name 16 -1)
            " " filename))))
+
+;; Smex
+(global-set-key (kbd "M-x") 'smex)
+ (global-set-key (kbd "M-X") 'smex-major-mode-commands)
+ (global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
--- local/ui.el
+++ local/ui.el
@@ -11,19 +11,10 @@
 ;; Theme
 (load-theme 'gruber-darker 1)
 
-(use-package mood-line
-  :ensure t
-  :config
-  (mood-line-mode 1)
-)
+;; Mode Line
+(mood-line-mode 1)
 
 ;; Font
-(defun get-default-font ()
-  (cond
-   ((eq system-type 'windows-nt) "Iosevka-12") ;; IosevkaTermSlab NF-12
-   ((eq system-type 'darwin) "Iosevka-14")
-   ((eq system-type 'gnu/linux) "Iosevka-12")))
-
 (add-to-list 'default-frame-alist `(font . ,(get-default-font)))
 
 ;; Ido Mode for Files
@@ -35,10 +26,6 @@
 
 ;; Ido Mode for M-x
 (smex-initialize)
-
-(global-set-key (kbd "M-x") 'smex)
- (global-set-key (kbd "M-X") 'smex-major-mode-commands)
- (global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
 
 ;; Line Numbering
 (global-display-line-numbers-mode 1)
@@ -54,12 +41,3 @@
 (setq make-backup-files nil)
 (setq auto-save-default nil)
 
-;; ---------------------------------------------------------------------------------
-;; -------- Tastatur ---------------------------------------------------------------
-;; ---------------------------------------------------------------------------------
-
-;; Deutsche Mac Tastatur
-(if (eq system-type 'darwin)
-    (setq mac-command-modifier 'meta
-	  mac-option-modifier 'none
-	  default-input-method "MacOSX"))
