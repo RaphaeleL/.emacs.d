3ecbd0c205d0759283f223a3daffed80b5202eee
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2024-08-15 17:54:38 +0200

    Work on Keymaps (to use emacs more..)

--- local/keymaps.el
+++ local/keymaps.el
@@ -1,3 +1,4 @@
+;; Helper Function
 (defun delete-current-line ()
   "Delete (not kill) the current line."
   (interactive)
@@ -10,8 +11,46 @@
 (fset 'yes-or-no-p 'y-or-n-p)
 (setq use-dialog-box nil)
 
-;; Swaping / Transposing Words
-(global-set-key (kbd "C-c t") 'transpose-words)
+;; Selection
+(global-set-key (kbd "M-w") 'mark-word)
+(global-set-key (kbd "M-a") 'mark-page)
+(global-set-key (kbd "M-F") 'mark-defun)
+(global-set-key (kbd "M-s") 'mark-paragraph)
+
+;; Jumping
+(global-set-key (kbd "C-c C-a") 'beginning-of-defun)
+(global-set-key (kbd "C-c C-e") 'end-of-defun)
+
+;; Misc
+(global-set-key (kbd "C-c C-n") 'duplicate-line)
+(global-set-key (kbd "C-c C-d") 'delete-current-line)
+(global-set-key (kbd "C-c C-j") 'join-line)
+(global-set-key (kbd "M-z") 'undo)
+
+;; Shorter Versions of given Keymaps
+(global-set-key (kbd "C-.") 'find-file)
+(global-set-key (kbd "C-d") 'dired)
+(global-set-key (kbd "C-g") 'magit)
+(global-set-key (kbd "C-s") 'save-buffer)
+(global-set-key (kbd "C-c j") 'join-line)
+(global-set-key (kbd "C-c n") 'duplicate-line)
+(global-set-key (kbd "C-c d") 'delete-current-line)
+(global-set-key (kbd "C-c a") 'beginning-of-defun)
+(global-set-key (kbd "C-c e") 'end-of-defun)
+
+;; Shell
+(global-set-key (kbd "C-c m") 'compile)
+(global-set-key (kbd "C-x m") 'compile)
+(global-set-key (kbd "C-c s") 'shell-command)
+
+;; Multi Cursor
+(global-set-key (kbd "C-<") 'mc/mark-next-like-this)
+(global-set-key (kbd "C->") 'mc/mark-previous-like-this)
+(global-set-key (kbd "M-m") 'mc/mark-more-like-this-extended)
+
+;; Font Size
+(global-set-key (kbd "M-+") (lambda () (interactive) (text-scale-increase 1)))
+(global-set-key (kbd "M--") (lambda () (interactive) (text-scale-decrease 1)))
 
 ;; Dired
 (global-set-key (kbd "C-x .") 'dired)
@@ -22,61 +61,18 @@
 (global-set-key (kbd "C-c g") 'magit-status)
 (global-set-key (kbd "C-c C-g") 'magit-log-all)
 
-;; Buffer Navigation
+;; Buffer 
 (global-set-key (kbd "C-<tab>") 'next-buffer)
 (global-set-key (kbd "C-<iso-lefttab>") 'previous-buffer)
 (global-set-key (kbd "C-c i") 'ibuffer)
-(global-set-key (kbd "C-c l") 'counsel-switch-buffer)  ;; switch-to-buffer
-
-;; Close window
-(global-set-key (kbd "C-c 0") 'delete-window)
-(global-set-key (kbd "C-c 1") 'delete-other-windows)
-
-;; Usefull Commands
-(global-set-key (kbd "C-c m") 'compile)
-(global-set-key (kbd "C-c s") 'shell-command)
-(global-set-key (kbd "C-c n") 'duplicate-line)
-(global-set-key (kbd "C-c d") 'delete-current-line)
-(global-set-key (kbd "C-c j") 'join-line)
+(global-set-key (kbd "C-c l") 'counsel-switch-buffer)
+(global-set-key (kbd "C-c k") (lambda () (interactive) (kill-current-buffer)))
 
 ;; Copy and Paste
 (simpleclip-mode 1)
 (global-set-key (kbd "C-c c") 'simpleclip-copy)
 (global-set-key (kbd "C-c v") 'simpleclip-paste)
 
-;; Multi Cursor
-(global-set-key (kbd "C-<") 'mc/mark-next-like-this)
-(global-set-key (kbd "C->") 'mc/mark-previous-like-this)
-
-;; Font Size
-(global-set-key (kbd "M-+") (lambda () (interactive) (text-scale-increase 1)))
-(global-set-key (kbd "M--") (lambda () (interactive) (text-scale-decrease 1)))
-
-;; Kill Current Buffer
-(global-set-key (kbd "C-c k") (lambda () (interactive) (kill-current-buffer)))
-
 ;; Move Text
 (global-set-key (kbd "M-p") 'move-text-up)
 (global-set-key (kbd "M-n") 'move-text-down)
-
-;; Undo
-(global-set-key (kbd "M-z") 'undo)
-
-;; Magit
-(global-set-key (kbd "C-c g") 'magit)
-
-;; Better Compile
-(global-set-key (kbd "C-x m") 'compile)
-
-;; Selection
-(global-set-key (kbd "M-w") 'mark-word)
-(global-set-key (kbd "M-a") 'mark-page)
-(global-set-key (kbd "M-F") 'mark-defun)
-(global-set-key (kbd "M-s") 'mark-paragraph)
-
-;; Jumping
-(global-set-key (kbd "C-c a") 'beginning-of-defun)
-(global-set-key (kbd "C-c e") 'end-of-defun)
-
-;; Line Numbers
-(global-set-key (kbd "C-c C-l") 'global-display-line-numbers-mode)
