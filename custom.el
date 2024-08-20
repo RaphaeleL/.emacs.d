0c580265777db1a8e80e184ee3a6aae562001068
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2024-08-20 11:29:30 +0200

    ok.

--- custom.el
+++ custom.el
@@ -1,40 +1,38 @@
+;;; -*- lexical-binding: t -*-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("9a977ddae55e0e91c09952e96d614ae0be69727ea78ca145beea1aae01ac78d2"
-     "99d1e29934b9e712651d29735dd8dcd431a651dfbe039df158aa973461af003e"
-     "7613ef56a3aebbec29618a689e47876a72023bbd1b8393efc51c38f5ed3f33d1"
-     "e27c9668d7eddf75373fa6b07475ae2d6892185f07ebed037eedf783318761d7"
-     "1ea82e39d89b526e2266786886d1f0d3a3fa36c87480fad59d8fab3b03ef576e"
+   '("9f297216c88ca3f47e5f10f8bd884ab24ac5bc9d884f0f23589b0a46a608fe14"
+     "9a977ddae55e0e91c09952e96d614ae0be69727ea78ca145beea1aae01ac78d2"
      default))
  '(package-selected-packages
-   '(auctex company consult counsel flycheck fontaine magit marginalia
-	    markdown-mode modus-themes mood-line move-text
-	    multiple-cursors orderless rainbow-delimiters rainbow-mode
+   '(auctex catppuccin-theme company consult counsel doom-themes flycheck
+	    magit marginalia markdown-mode modus-themes mood-line
+	    move-text multiple-cursors orderless rainbow-delimiters
 	    simpleclip spacious-padding vertico web-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right. 
- '(fringe ((t :background "#fbf7f0")))
- '(header-line ((t :box (:line-width 4 :color "#efe9dd" :style nil))))
- '(header-line-highlight ((t :box (:color "#000000"))))
+ ;; If there is more than one, they won't work right.
+ '(fringe ((t :background "White")))
+ '(header-line ((t :box (:line-width 4 :color "grey90" :style nil))))
+ '(header-line-highlight ((t :box (:color "Black"))))
  '(keycast-key ((t)))
- '(line-number ((t :background "#fbf7f0")))
- '(mode-line ((t :box (:line-width 6 :color "#cab9b2" :style nil))))
- '(mode-line-active ((t :box (:line-width 6 :color "#cab9b2" :style nil))))
- '(mode-line-highlight ((t :box (:color "#000000"))))
- '(mode-line-inactive ((t :box (:line-width 6 :color "#dfd9cf" :style nil))))
- '(tab-bar-tab ((t :box (:line-width 4 :color "#fbf7f0" :style nil))))
- '(tab-bar-tab-inactive ((t :box (:line-width 4 :color "#c8b8b2" :style nil))))
+ '(line-number ((t :background "White")))
+ '(mode-line ((t :box (:line-width 6 :color "grey75" :style nil))))
+ '(mode-line-active ((t :box (:line-width 6 :color "grey75" :style nil))))
+ '(mode-line-highlight ((t :box (:color "Black"))))
+ '(mode-line-inactive ((t :box (:line-width 6 :color "grey90" :style nil))))
+ '(tab-bar-tab ((t :box (:line-width 4 :color "grey85" :style nil))))
+ '(tab-bar-tab-inactive ((t :box (:line-width 4 :color "grey75" :style nil))))
  '(tab-line-tab ((t)))
  '(tab-line-tab-active ((t)))
  '(tab-line-tab-inactive ((t)))
- '(vertical-border ((t :background "#fbf7f0" :foreground "#fbf7f0")))
- '(window-divider ((t (:background "#fbf7f0" :foreground "#fbf7f0"))))
- '(window-divider-first-pixel ((t (:background "#fbf7f0" :foreground "#fbf7f0"))))
- '(window-divider-last-pixel ((t (:background "#fbf7f0" :foreground "#fbf7f0")))))
+ '(vertical-border ((t :background "White" :foreground "White")))
+ '(window-divider ((t (:background "White" :foreground "White"))))
+ '(window-divider-first-pixel ((t (:background "White" :foreground "White"))))
+ '(window-divider-last-pixel ((t (:background "White" :foreground "White")))))
