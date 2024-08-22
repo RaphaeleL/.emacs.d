001cbb1c584fbe4cc28f61e0f4ce916390cfbb01
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2024-08-22 20:48:39 +0200

    Updates

--- custom.el
+++ custom.el
@@ -5,13 +5,14 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("9a977ddae55e0e91c09952e96d614ae0be69727ea78ca145beea1aae01ac78d2"
+   '("e13beeb34b932f309fb2c360a04a460821ca99fe58f69e65557d6c1b10ba18c7"
+     "9a977ddae55e0e91c09952e96d614ae0be69727ea78ca145beea1aae01ac78d2"
      default))
  '(package-selected-packages
-   '(company counsel flycheck magit marginalia modus-themes mood-line
-	     move-text multiple-cursors orderless org-bullets
-	     perfect-margin simpleclip spacious-padding vertico
-	     web-mode)))
+   '(company counsel flycheck gruber-darker-theme magit marginalia
+	     modus-themes mood-line move-text multiple-cursors
+	     orderless org-bullets perfect-margin simpleclip
+	     spacious-padding vertico web-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
