2e91b5ef55c669c47ec567a0b4f38413969fdfc6
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2024-08-22 20:39:32 +0200

    Rewrite into Org

--- custom.el
+++ custom.el
@@ -5,35 +5,37 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("9f297216c88ca3f47e5f10f8bd884ab24ac5bc9d884f0f23589b0a46a608fe14"
-     "9a977ddae55e0e91c09952e96d614ae0be69727ea78ca145beea1aae01ac78d2"
+   '("9a977ddae55e0e91c09952e96d614ae0be69727ea78ca145beea1aae01ac78d2"
      default))
  '(package-selected-packages
-   '(auctex catppuccin-theme company consult counsel doom-themes flycheck
-	    magit marginalia markdown-mode modus-themes mood-line
-	    move-text multiple-cursors orderless perfect-margin
-	    rainbow-delimiters simpleclip spacious-padding vertico
-	    web-mode)))
+   '(company counsel flycheck magit marginalia modus-themes mood-line
+	     move-text multiple-cursors orderless org-bullets
+	     perfect-margin simpleclip spacious-padding vertico
+	     web-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(fringe ((t :background "White")))
- '(header-line ((t :box (:line-width 4 :color "grey90" :style nil))))
- '(header-line-highlight ((t :box (:color "Black"))))
+ '(fringe ((t :background "#ffffff")))
+ '(header-line ((t :box (:line-width 4 :color "#f2f2f2" :style nil))))
+ '(header-line-highlight ((t :box (:color "#000000"))))
  '(keycast-key ((t)))
- '(line-number ((t :background "White")))
- '(mode-line ((t :box (:line-width 6 :color "grey75" :style nil))))
- '(mode-line-active ((t :box (:line-width 6 :color "grey75" :style nil))))
- '(mode-line-highlight ((t :box (:color "Black"))))
- '(mode-line-inactive ((t :box (:line-width 6 :color "grey90" :style nil))))
- '(tab-bar-tab ((t :box (:line-width 4 :color "grey85" :style nil))))
- '(tab-bar-tab-inactive ((t :box (:line-width 4 :color "grey75" :style nil))))
+ '(line-number ((t :background "#ffffff")))
+ '(mode-line ((t :box (:line-width 6 :color "#c8c8c8" :style nil))))
+ '(mode-line-active ((t :box (:line-width 6 :color "#c8c8c8" :style nil))))
+ '(mode-line-highlight ((t :box (:color "#000000"))))
+ '(mode-line-inactive ((t :box (:line-width 6 :color "#e6e6e6" :style nil))))
+ '(org-level-1 ((t (:inherit default :weight bold :height 1.6))))
+ '(org-level-2 ((t (:inherit default :weight bold :height 1.4))))
+ '(org-level-3 ((t (:inherit default :weight bold :height 1.2))))
+ '(org-level-4 ((t (:inherit default :weight bold :height 1.0))))
+ '(tab-bar-tab ((t :box (:line-width 4 :color "#ffffff" :style nil))))
+ '(tab-bar-tab-inactive ((t :box (:line-width 4 :color "#c2c2c2" :style nil))))
  '(tab-line-tab ((t)))
  '(tab-line-tab-active ((t)))
  '(tab-line-tab-inactive ((t)))
- '(vertical-border ((t :background "White" :foreground "White")))
- '(window-divider ((t (:background "White" :foreground "White"))))
- '(window-divider-first-pixel ((t (:background "White" :foreground "White"))))
- '(window-divider-last-pixel ((t (:background "White" :foreground "White")))))
+ '(vertical-border ((t :background "#ffffff" :foreground "#ffffff")))
+ '(window-divider ((t (:background "#ffffff" :foreground "#ffffff"))))
+ '(window-divider-first-pixel ((t (:background "#ffffff" :foreground "#ffffff"))))
+ '(window-divider-last-pixel ((t (:background "#ffffff" :foreground "#ffffff")))))
--- init.el
+++ init.el
@@ -1,11 +1,4 @@
-(package-initialize)
-
-(add-to-list 'load-path "~/.emacs.d/general/")
-
-(load "~/.emacs.d/general/packages.el")
-(load "~/.emacs.d/general/keymaps.el")
-(load "~/.emacs.d/general/settings.el")
-(load "~/.emacs.d/general/utils.el")
-(load "~/.emacs.d/general/ui.el")
-(load "~/.emacs.d/general/lsp.el")
-(load "~/.emacs.d/general/modes.el")
+(org-babel-load-file
+ (expand-file-name
+  "config.org"
+  user-emacs-directory))
