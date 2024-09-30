bd602756d127b5954d82bbaeb09cb9dfc78cbf47
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2024-09-30 13:04:57 +0200

    Performance
    1.51s -> 0.61s

--- init.el
+++ init.el
@@ -8,7 +8,6 @@
       '(("melpa" . "https://melpa.org/packages/")
         ("gnu" . "https://elpa.gnu.org/packages/")
         ("org" . "https://orgmode.org/elpa/")))
-(package-refresh-contents)
 (package-initialize)
 
 ; ==================================================
@@ -92,7 +91,6 @@
 ; ===== PACKAGES ===================================
 ; ==================================================
 
-(rc/require 'markdown-mode)
 (rc/require 'which-key)
 (rc/require 'eglot)
 (rc/require 'company)
@@ -284,4 +282,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
-)
+ )
