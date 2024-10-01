8db14320d48b98223a31a71be260cb4280596bbe
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2024-10-01 09:15:17 +0200

    Typo
    Signed-off-by: Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>

--- init.el
+++ init.el
@@ -183,7 +183,7 @@
 (setq-default tab-width 4)
 
 ; ==================================================
-; ===== MACOS STUFFK ===============================
+; ===== MACOS STUFF ================================
 ; ==================================================
 
 (if (eq system-type 'darwin)
@@ -282,4 +282,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+)
