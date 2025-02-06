a5314aa51332cf4d65f5105aa29fba887bdbabae
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2025-02-06 06:41:08 +0100

    Auto Detect Emacs Config for each OS.
    - Auto Detect
    - Updates on Linux Config

--- init.el
+++ init.el
@@ -1,23 +1,4 @@
-;; ============================================================
-;; ========= Base Config ======================================
-;; ============================================================
-
-(load "~/.emacs.d/_base/melpa.el")
-(load "~/.emacs.d/_base/functions.el")
-(load "~/.emacs.d/_base/packages.el")
-; (load "~/.emacs.d/_base/garbage.el")
-(load "~/.emacs.d/_base/ui.el")
-(load "~/.emacs.d/_base/base.el")
-(load "~/.emacs.d/_base/basic.el")
-; (load "~/.emacs.d/_base/comp-buffer.el")
-(load "~/.emacs.d/_base/minibuffer.el")
-(load "~/.emacs.d/_base/package-settings.el")
-; (load "~/.emacs.d/_base/mac.el")
-(load "~/.emacs.d/_base/keymaps.el")
-(load "~/.emacs.d/_base/lsp.el")
-; (load "~/.emacs.d/_base/ssh.el")
-(load "~/.emacs.d/_base/custom.el")
-
-;; ============================================================
-;; ========= Custom Adjustments ===============================
-;; ============================================================
+(cond
+ ((eq system-type 'windows-nt) (load "~/.emacs.d/_templates/init.windows.el"))
+ ((eq system-type 'darwin) (load "~/.emacs.d/_templates/init.mac.el"))
+ ((eq system-type 'gnu/linux) (load "~/.emacs.d/_templates/init.linux.el")))
