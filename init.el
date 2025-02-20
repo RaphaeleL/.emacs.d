dd90b0fd724fb8dd79285336a974e30fbb556f6b
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2025-02-20 08:04:48 +0100

    Adjustments

--- init.el
+++ init.el
@@ -1,4 +1,14 @@
+;; ===============================================
+;; === BASE CONFIGURATION BASED ON THE OS ========
+;; ===============================================
+
 (cond
  ((eq system-type 'windows-nt) (load "~/.emacs.d/_templates/init.windows.el"))
- ((eq system-type 'darwin) (load "~/.emacs.d/_templates/init.mac.el"))
- ((eq system-type 'gnu/linux) (load "~/.emacs.d/_templates/init.linux.el")))
+ ((eq system-type 'darwin)     (load "~/.emacs.d/_templates/init.mac.el"))
+ ((eq system-type 'gnu/linux)  (load "~/.emacs.d/_templates/init.linux.el")))
+
+;; ===============================================
+;; === CUSTOM ADJUSTMENTS ========================
+;; ===============================================
+
+
