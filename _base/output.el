a1a6979e0bdfc5199514e3735ec41d4d933992d4
Raphaele Licciardo <raphaele.licciardo@frequentis.com>
2025-03-05 14:18:28 +0100

    Update README.md

--- _base/output.el
+++ _base/output.el
@@ -11,6 +11,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(dired-directory ((t (:foreground "olive drab" :weight bold))))
  '(dired-header ((t (:foreground "base01" :background "base03" :weight normal))))
  '(magit-header-line ((t (:background "old lace" :foreground "slate gray" :box (:line-width (1 . 1) :color "old lace") :weight bold))))
  '(magit-section-heading ((t (:extend t :foreground "black" :weight bold)))))
