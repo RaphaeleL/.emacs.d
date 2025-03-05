eb8235737c4b6ba01775185cd6d174c469c0448d
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2025-03-05 07:06:15 +0100

    Try out other Theme

--- _base/output.el
+++ _base/output.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(rpm-spec-mode dockerfile-mode doom-themes markdown-mode ssh theme-changer jenkinsfile-mode xterm-color groovy-mode magit multiple-cursors move-text simpleclip gruber-darker-theme)))
+   '(lab-themes flucui-themes solarized-theme rpm-spec-mode dockerfile-mode doom-themes markdown-mode ssh theme-changer jenkinsfile-mode xterm-color groovy-mode magit multiple-cursors move-text simpleclip gruber-darker-theme)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
