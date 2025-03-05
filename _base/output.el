69b8e046e66c45f70eff43debf42c21552a8ec61
Raphaele Licciardo <raphaele.licciardo@frequentis.com>
2025-03-05 12:37:31 +0100

    Doom's Solarized

--- _base/output.el
+++ _base/output.el
@@ -3,11 +3,14 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(mini-frame-show-parameters '((top . 10) (width . 0.7) (left . 0.5)))
  '(package-selected-packages
-   '(lab-themes flucui-themes solarized-theme rpm-spec-mode dockerfile-mode doom-themes markdown-mode ssh theme-changer jenkinsfile-mode xterm-color groovy-mode magit multiple-cursors move-text simpleclip gruber-darker-theme)))
+   '(web-mode lab-themes flucui-themes rpm-spec-mode dockerfile-mode doom-themes markdown-mode ssh theme-changer jenkinsfile-mode xterm-color groovy-mode magit multiple-cursors move-text simpleclip gruber-darker-theme)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(dired-header ((t (:foreground "base01" :background "base03" :weight normal))))
+ '(magit-header-line ((t (:background "old lace" :foreground "slate gray" :box (:line-width (1 . 1) :color "old lace") :weight bold))))
+ '(magit-section-heading ((t (:extend t :foreground "black" :weight bold)))))
--- _base/ui.el
+++ _base/ui.el
@@ -24,16 +24,12 @@
 ; (set-face-attribute 'mode-line nil :box nil)
 ; (load-theme 'doom-one-light 1)
 ; (load-theme 'gruber-darker t)
-; (load-theme 'doom-solarized-light t)
-(load-theme 'solarized-light t)
+(load-theme 'doom-solarized-light t)
 
-(eval-after-load 'dired
-  '(custom-set-faces
-    '(dired-header ((t (:foreground "base01" :background "base03" :weight normal))))))
-
-;; (solarized-create-theme-file-with-palette 'light 'solarized-light-custom
-;;   '("#000000" "#faf5ee"
-;;     "#3388dd" "#ac3d1a" "#dd2222" "#8b008b" "#00b7f0" "#1388a2" "#104e8b" "#00688b"))
+'(custom-set-faces
+ '(dired-header ((t (:foreground "base01" :background "base03" :weight normal))))
+ '(magit-header-line ((t (:background "old lace" :foreground "slate gray" :weight bold))))
+ '(magit-section-heading ((t (:extend t :foreground "black" :weight bold)))))
 
 (blink-cursor-mode 0)
 (setq x-stretch-cursor nil)
