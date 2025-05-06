681d8a6294fd44115925d09ba4ccef917c666d71
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2025-05-06 07:27:28 +0200

    Update on Keymaps
    - Tsoding Way on Multiple Cursor
    - Indent

--- _base/keymaps.el
+++ _base/keymaps.el
@@ -3,62 +3,64 @@
 ; ==================================================
 
 ;; Selecting Stuff
-(global-set-key (kbd "M-w")     'mark-word)
-(global-set-key (kbd "M-a")     'mark-page)
-(global-set-key (kbd "M-F")     'mark-defun)
-(global-set-key (kbd "M-s")     'mark-paragraph)
+(global-set-key (kbd "M-w")         'mark-word)
+(global-set-key (kbd "M-a")         'mark-page)
+(global-set-key (kbd "M-F")         'mark-defun)
+(global-set-key (kbd "M-s")         'mark-paragraph)
 
 ;; No Description needed
-(global-set-key (kbd "M-j")     'join-line)
-(global-set-key (kbd "M-z")     'undo)
-(global-set-key (kbd "M-d")     'rc/duplicate-line)
-(global-set-key (kbd "M-r")     'rc/delete-line)
+(global-set-key (kbd "M-j")         'join-line)
+(global-set-key (kbd "M-z")         'undo)
+(global-set-key (kbd "M-d")         'rc/duplicate-line)
+(global-set-key (kbd "M-r")         'rc/delete-line)
 
 ;; Default Emacs stuff, just simpler
-(global-set-key (kbd "C-,")     'find-file)
-(global-set-key (kbd "C-.")     'dired)
-(global-set-key (kbd "C-c C-,") 'project-find-file)
-(global-set-key (kbd "C-c g")   'magit-status)
+(global-set-key (kbd "C-,")         'find-file)
+(global-set-key (kbd "C-.")         'dired)
+(global-set-key (kbd "C-c C-,")     'project-find-file)
+(global-set-key (kbd "C-c g")       'magit-status)
 
 ;; Shell Commands
-(global-set-key (kbd "C-c m")   'compile)
-(global-set-key (kbd "C-c s")   'shell-command)
+(global-set-key (kbd "C-c m")       'compile)
+(global-set-key (kbd "C-c s")       'shell-command)
 
 ;; Buffer Management
-(global-set-key (kbd "M-i")     'ibuffer)
-(global-set-key (kbd "M-o")     'switch-to-buffer)
-(global-set-key (kbd "M-e")     'next-buffer)
-(global-set-key (kbd "M-q")     'previous-buffer)
+(global-set-key (kbd "M-i")         'ibuffer)
+(global-set-key (kbd "M-o")         'switch-to-buffer)
+(global-set-key (kbd "M-e")         'next-buffer)
+(global-set-key (kbd "M-q")         'previous-buffer)
 
 ;; Copy and Paste
-(global-set-key (kbd "C-w")     'rc/copy)
-(global-set-key (kbd "C-y")     'rc/paste)
-(global-set-key (kbd "C-t")     'rc/cut)
+(global-set-key (kbd "C-w")         'rc/copy)
+(global-set-key (kbd "C-y")         'rc/paste)
+(global-set-key (kbd "C-t")         'rc/cut)
 
 ;; Moving a Selection up and down
-(global-set-key (kbd "M-p")     'move-text-up)
-(global-set-key (kbd "M-n")     'move-text-down)
+(global-set-key (kbd "M-p")         'move-text-up)
+(global-set-key (kbd "M-n")         'move-text-down)
 
 ;; Multi line Editing
-(global-set-key (kbd "M-/")     'mc/mark-next-like-this)
-(global-set-key (kbd "C-j")     'mc/mark-next-like-this)
-(global-set-key (kbd "C-k")     'mc/mark-previous-like-this)
-(global-set-key (kbd "M-m")     'mc/mark-more-like-this-extended)
+(global-set-key (kbd "C-c C-SPC")   'rectangle-mark-mode)
+(global-set-key (kbd "C-c C-i")     'mc/edit-lines)
+(global-set-key (kbd "M-/")         'mc/mark-next-like-this)
+(global-set-key (kbd "C-j")         'mc/mark-next-like-this)
+(global-set-key (kbd "C-k")         'mc/mark-previous-like-this)
+(global-set-key (kbd "M-m")         'mc/mark-more-like-this-extended)
 
 ;; Stuff i do not need daily, but i definitely want a shortcut for
-(global-set-key (kbd "<f1>")    (lambda () (interactive) (rc/toggle-buffer "*scratch*")))
-(global-set-key (kbd "<f2>")    (lambda () (interactive) (rc/toggle-buffer "*compilation*")))
-(global-set-key (kbd "<f3>")    'rc/toggle-themes)
-(global-set-key (kbd "<f4>")    (lambda () (interactive) (find-file "~/.emacs.d/init.el")))
-(global-set-key (kbd "<f5>")    'rc/toggle-mini-buffer-mode)
+(global-set-key (kbd "<f1>")        (lambda () (interactive) (rc/toggle-buffer "*scratch*")))
+(global-set-key (kbd "<f2>")        (lambda () (interactive) (rc/toggle-buffer "*compilation*")))
+(global-set-key (kbd "<f3>")        'rc/toggle-themes)
+(global-set-key (kbd "<f4>")        (lambda () (interactive) (find-file "~/.emacs.d/init.el")))
+(global-set-key (kbd "<f5>")        'rc/toggle-mini-buffer-mode)
 
 ;; Zooming
-(global-set-key (kbd "C-=")     (lambda () (interactive) (text-scale-increase 1)))
-(global-set-key (kbd "C-+")     (lambda () (interactive) (text-scale-increase 1)))
-(global-set-key (kbd "C--")     (lambda () (interactive) (text-scale-decrease 1)))
+(global-set-key (kbd "C-=")         (lambda () (interactive) (text-scale-increase 1)))
+(global-set-key (kbd "C-+")         (lambda () (interactive) (text-scale-increase 1)))
+(global-set-key (kbd "C--")         (lambda () (interactive) (text-scale-decrease 1)))
 
-(global-set-key (kbd "M-=")     'global-text-scale-adjust)
-(global-set-key (kbd "M-+")     'global-text-scale-adjust)
+(global-set-key (kbd "M-=")         'global-text-scale-adjust)
+(global-set-key (kbd "M-+")         'global-text-scale-adjust)
 
 
 
