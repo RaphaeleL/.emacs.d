24eee506e6b73c4da0bcd88e5dfdb5773ea696e3
LICCIARDO Raphaele Salvatore <raphaele.licciardo@frequentis.com>
2025-06-12 15:03:48 +0200

    Unify Things

--- _base/ssh.el
+++ _base/ssh.el
@@ -6,7 +6,6 @@
 (setq tramp-verbose 10)
 (setq tramp-ssh-controlmaster-options "")
 
-; (defun enzo () (interactive) (find-file "/plink:raliccia@10.26.15.66:/home/raliccia/"))
 (defun ksbuild8 () (interactive) (find-file "/plink:raliccia@ksbuild8:/home_fco/raliccia/"))
 
 (defun nikos () (interactive) (find-file "/plink:root@testvm1:/"))
@@ -15,13 +14,13 @@
 (defun sander () (interactive) (find-file "/plink:root@testvm4:/"))
 (defun paola () (interactive) (find-file "/plink:root@testvm5:/"))
 
-; (global-set-key (kbd "C-x C-e") 'ksbuild8)
-; (global-set-key (kbd "C-x C-r") 'enzo)
-
-(global-set-key (kbd "C-c 0") 'enzo)
 (global-set-key (kbd "C-c 1") 'nikos)
 (global-set-key (kbd "C-c 2") 'zeudi)
 (global-set-key (kbd "C-c 3") 'giulia)
 (global-set-key (kbd "C-c 4") 'sander)
 (global-set-key (kbd "C-c 5") 'paola)
 (global-set-key (kbd "C-c 8") 'ksbuild8)
+
+(when (eq system-type 'windows-nt)
+  (defun enzo () (interactive) (find-file "/plink:raliccia@10.26.15.66:/home/raliccia/"))
+  (global-set-key (kbd "C-c 0") 'enzo))
