6b491332bd00d576ddd976dd2ccf9f97424ec260
LICCIARDO Raphaele Salvatore <raphaele.licciardo@frequentis.com>
2025-06-12 14:47:40 +0200

    Update SSH

--- _base/ssh.el
+++ _base/ssh.el
@@ -2,14 +2,26 @@
 ; ====== SSH =======================================
 ; ==================================================
 
-(setq tramp-default-method "ssh")
-(setq tramp-ssh-controlmaster-options "")
+(setq tramp-default-method "plink")
 (setq tramp-verbose 10)
-(defun open-devvm ()
-  (interactive)
-  (find-file "/plink:raliccia@10.26.15.66:/home/raliccia/"))
-(defun open-ksbuild8 ()
-  (interactive)
-  (find-file "/plink:raliccia@ksbuild8:/home_fco/raliccia/"))
-(global-set-key (kbd "C-x C-r") 'open-devvm)
-(global-set-key (kbd "C-x C-e") 'open-ksbuild8)
+(setq tramp-ssh-controlmaster-options "")
+
+; (defun enzo () (interactive) (find-file "/plink:raliccia@10.26.15.66:/home/raliccia/"))
+(defun ksbuild8 () (interactive) (find-file "/plink:raliccia@ksbuild8:/home_fco/raliccia/"))
+
+(defun nikos () (interactive) (find-file "/plink:root@testvm1:/"))
+(defun zeudi () (interactive) (find-file "/plink:root@testvm2:/"))
+(defun giulia () (interactive) (find-file "/plink:root@testvm3:/"))
+(defun sander () (interactive) (find-file "/plink:root@testvm4:/"))
+(defun paola () (interactive) (find-file "/plink:root@testvm5:/"))
+
+; (global-set-key (kbd "C-x C-e") 'ksbuild8)
+; (global-set-key (kbd "C-x C-r") 'enzo)
+
+(global-set-key (kbd "C-c 0") 'enzo)
+(global-set-key (kbd "C-c 1") 'nikos)
+(global-set-key (kbd "C-c 2") 'zeudi)
+(global-set-key (kbd "C-c 3") 'giulia)
+(global-set-key (kbd "C-c 4") 'sander)
+(global-set-key (kbd "C-c 5") 'paola)
+(global-set-key (kbd "C-c 8") 'ksbuild8)
