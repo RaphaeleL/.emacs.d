0dfb287edeb0511627ed3faa3333d4e57d05118c
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2025-06-17 16:48:40 +0200

    MacOS Updates

--- _base/mac.el
+++ _base/mac.el
@@ -13,6 +13,12 @@
   ;; Use GNU ls from coreutils (via Homebrew)
   (setq insert-directory-program "/opt/homebrew/bin/gls"))
 
-
+;; Homebrew
 (when (eq system-type 'darwin)
   (setq insert-directory-program "/opt/homebrew/bin/gls"))
+
+;; WTF
+(global-set-key (kbd "C-c 8") (lambda () (interactive) (insert "{")))
+(global-set-key (kbd "C-c 9") (lambda () (interactive) (insert "}")))
+(global-set-key (kbd "C-c 5") (lambda () (interactive) (insert "[")))
+(global-set-key (kbd "C-c 6") (lambda () (interactive) (insert "]")))
