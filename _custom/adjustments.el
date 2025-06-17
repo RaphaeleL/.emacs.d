df69899759c80181f7bb13a859bce6df7e9e0fc3
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2025-06-17 13:15:56 +0200

    Try out Treesitter for Python and Yaml Files

--- _custom/adjustments.el
+++ _custom/adjustments.el
@@ -1,3 +1,20 @@
 ;; ===============================================
 ;; === CUSTOM ADJUSTMENTS ========================
 ;; ===============================================
+
+;; === TREESITTER ================================
+
+(require 'treesit)
+
+(setq treesit-language-source-alist
+      '((python . ("https://github.com/tree-sitter/tree-sitter-python"))
+        (yaml   . ("https://github.com/ikatyang/tree-sitter-yaml"))))
+
+(dolist (lang '(python yaml))
+  (unless (treesit-ready-p lang)
+    (message "Installing tree-sitter grammar for %s..." lang)
+    (treesit-install-language-grammar lang)))
+
+(setq major-mode-remap-alist
+      '((python-mode . python-ts-mode)
+        (yaml-mode   . yaml-ts-mode)))
