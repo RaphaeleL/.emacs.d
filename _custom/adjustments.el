1255e67479c5096fb447687ade4b1850d85ffdfc
LICCIARDO Raphaele Salvatore <raphaele.licciardo@frequentis.com>
2025-06-18 11:24:46 +0200

    Update Adjustments

--- _custom/adjustments.el
+++ _custom/adjustments.el
@@ -3,29 +3,26 @@
 ;; ===============================================
 
 ;; === TREESITTER ================================
-
-(require 'treesit)
-
 ; TODO: Improve the Flow, its shitty :D
+(require 'treesit)
 (setq treesit-language-source-alist
       '((python . ("https://github.com/tree-sitter/tree-sitter-python"))
         (yaml   . ("https://github.com/ikatyang/tree-sitter-yaml"))))
-
 (dolist (lang '(python yaml))
   (unless (treesit-ready-p lang)
     (message "Installing tree-sitter grammar for %s..." lang)
     (treesit-install-language-grammar lang)))
-
 (setq major-mode-remap-alist
       '((python-mode . python-ts-mode)
         (yaml-mode   . yaml-ts-mode)))
 
 ;; === I PREFER THE BARE UI ======================
-
 (when (eq system-type 'windows-nt)
   (rc/bare))
 
 ;; === DEFAULT DIR ON WINDOWS ====================
-
 (when (eq system-type 'windows-nt)
   (setq default-directory "C:/Users/RaLiccia/"))
+
+;; === NO MORE MODELINE ==========================
+; (setq-default mode-line-format nil)
