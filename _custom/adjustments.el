4989c38f60fb3572d54a18dc3c0719bc13517de6
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2025-06-18 08:12:10 +0200

    Adjust Custom

--- _custom/adjustments.el
+++ _custom/adjustments.el
@@ -4,10 +4,9 @@
 
 ;; === TREESITTER ================================
 
-; TODO: Improve the Flow, its shitty :D
-
 (require 'treesit)
 
+; TODO: Improve the Flow, its shitty :D
 (setq treesit-language-source-alist
       '((python . ("https://github.com/tree-sitter/tree-sitter-python"))
         (yaml   . ("https://github.com/ikatyang/tree-sitter-yaml"))))
@@ -20,3 +19,13 @@
 (setq major-mode-remap-alist
       '((python-mode . python-ts-mode)
         (yaml-mode   . yaml-ts-mode)))
+
+;; === I PREFER THE BARE UI ======================
+
+(when (eq system-type 'windows-nt)
+  (rc/bare))
+
+;; === DEFAULT DIR ON WINDOWS ====================
+
+(when (eq system-type 'windows-nt)
+  (setq default-directory "C:/Users/RaLiccia/"))
