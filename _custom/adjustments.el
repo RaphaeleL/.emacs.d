e4fc3ff75bf96750098b7b2eb0237fdea7bc3cbe
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2025-06-20 07:49:20 +0200

    Adjustments

--- _custom/adjustments.el
+++ _custom/adjustments.el
@@ -3,25 +3,28 @@
 ;; ===============================================
 
 ;; === TREESITTER ================================
-(require 'treesit)
-(setq treesit-language-source-alist
-      '((python . ("https://github.com/tree-sitter/tree-sitter-python"))
-        (yaml   . ("https://github.com/ikatyang/tree-sitter-yaml"))))
-(dolist (lang '(python yaml))
-  (unless (treesit-ready-p lang)
-    (message "Installing tree-sitter grammar for %s..." lang)
-    (treesit-install-language-grammar lang)))
-(setq major-mode-remap-alist
-      '((python-mode . python-ts-mode)
-        (yaml-mode   . yaml-ts-mode)))
+;; (require 'treesit)
+;; (setq treesit-language-source-alist
+;;       '((python . ("https://github.com/tree-sitter/tree-sitter-python"))
+;;         (yaml   . ("https://github.com/ikatyang/tree-sitter-yaml"))))
+;; (dolist (lang '(python yaml))
+;;   (unless (treesit-ready-p lang)
+;;     (message "Installing tree-sitter grammar for %s..." lang)
+;;     (treesit-install-language-grammar lang)))
+;; (setq major-mode-remap-alist
+;;       '((python-mode . python-ts-mode)
+;;         (yaml-mode   . yaml-ts-mode)))
 
-;; === I PREFER THE BARE UI ======================
-; (rc/bare)
+;; === MACOS =====================================
 (when (eq system-type 'darwin)
   (rc/disable-custom-font)
   (set-face-attribute 'default nil :height 160))
-; (setq-default mode-line-format nil)
 
-;; === DEFAULT DIR ON WINDOWS ====================
+;; === WINDOWS =====================================
 (when (eq system-type 'windows-nt)
   (setq default-directory "C:/Users/RaLiccia/"))
+
+;; === GNU/LINUX ===================================
+(when (eq system-type 'gnu/linux)
+  (rc/disable-custom-font)
+  (set-face-attribute 'default nil :height 160))
