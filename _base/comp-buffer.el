f568c87758ed50e1ad0cb037008c0ec7681d0520
LICCIARDO Raphaele Salvatore <raphaele.licciardo@frequentis.com>
2025-06-20 15:27:22 +0200

    Major Updates

--- _base/comp-buffer.el
+++
@@ -1,8 +0,0 @@
-; ==================================================
-; ===== COLORIZE THE COMPILATION BUFFER ============
-; ==================================================
-
-(setq compilation-environment '("TERM=xterm-256color"))
-(defun my/advice-compilation-filter (f proc string)
-  (funcall f proc (xterm-color-filter string)))
-(advice-add 'compilation-filter :around #'my/advice-compilation-filter)
