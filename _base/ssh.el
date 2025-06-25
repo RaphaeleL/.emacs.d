c40ae939d1791d1c171a3a717a164253fc03ab80
Raphaele Licciardo <raphaele.licciardo@frequentis.com>
2025-06-25 05:56:52 +0200

    Unify ssh.

--- _base/ssh.el
+++ _base/ssh.el
@@ -6,11 +6,16 @@
 (setq tramp-verbose 10)
 (setq tramp-ssh-controlmaster-options "")
 
+(defun enzo () (interactive)
+  (progn
+  (dired "/plink:raliccia@10.26.15.66:/home/raliccia/")
+  (dired "/plink:raliccia@10.26.15.66:/home/raliccia/dev/fco/develop/ccms")))
+
 (defun ksbuild8 ()
   (interactive)
   (progn
-    (find-file "/plink:raliccia@ksbuild8:/home_fco/raliccia/")
-    (find-file "/plink:raliccia@ksbuild8:/home_fco/raliccia/paths.txt")
+    (dired "/plink:raliccia@ksbuild8:/home_fco/raliccia/")
+    (dired "/plink:raliccia@ksbuild8:/home_fco/raliccia/paths.txt")
     (dired "/plink:raliccia@ksbuild8:/home1/products/ccms/kickstarts/raliccia/")
     (dired "/plink:raliccia@ksbuild8:/home1/tmp/raliccia/")
     (dired "/plink:raliccia@ksbuild8:/home1/kickstart_iso/nis/ccms/rlicciar/")))
@@ -18,7 +23,7 @@
 (defun nikos ()
   (interactive)
   (progn
-    (find-file "/plink:root@testvm1:/")
+    (dired "/plink:root@testvm1:/")
     (dired "/plink:root@testvm1:/var/log/ccms/")
     (dired "/plink:root@testvm1:/usr/share/ccms/ansible/")
     (dired "/plink:root@testvm1:/usr/lib/python3.12/site-packages/ccms/")
@@ -27,7 +32,7 @@
 (defun zeudi ()
   (interactive)
   (progn
-    (find-file "/plink:root@testvm2:/")
+    (dired "/plink:root@testvm2:/")
     (dired "/plink:root@testvm2:/var/log/ccms/")
     (dired "/plink:root@testvm2:/usr/share/ccms/ansible/")
     (dired "/plink:root@testvm2:/usr/lib/python3.12/site-packages/ccms/")
@@ -36,7 +41,7 @@
 (defun giulia ()
   (interactive)
   (progn
-    (find-file "/plink:root@testvm3:/")
+    (dired "/plink:root@testvm3:/")
     (dired "/plink:root@testvm3:/var/log/ccms/")
     (dired "/plink:root@testvm3:/usr/share/ccms/ansible/")
     (dired "/plink:root@testvm3:/usr/lib/python3.12/site-packages/ccms/")
@@ -45,7 +50,7 @@
 (defun sander ()
   (interactive)
   (progn
-    (find-file "/plink:root@testvm4:/")
+    (dired "/plink:root@testvm4:/")
     (dired "/plink:root@testvm4:/var/log/ccms/")
     (dired "/plink:root@testvm4:/usr/share/ccms/ansible/")
     (dired "/plink:root@testvm4:/usr/lib/python3.12/site-packages/ccms/")
@@ -54,19 +59,16 @@
 (defun paola ()
   (interactive)
   (progn
-    (find-file "/plink:root@testvm5:/")
+    (dired "/plink:root@testvm5:/")
     (dired "/plink:root@testvm5:/var/log/ccms/")
     (dired "/plink:root@testvm5:/usr/share/ccms/ansible/")
     (dired "/plink:root@testvm5:/usr/lib/python3.12/site-packages/ccms/")
     (dired "/plink:root@testvm5:/etc/ccms/")))
 
+(global-set-key (kbd "C-c 0") 'enzo)
 (global-set-key (kbd "C-c 1") 'nikos)
 (global-set-key (kbd "C-c 2") 'zeudi)
 (global-set-key (kbd "C-c 3") 'giulia)
 (global-set-key (kbd "C-c 4") 'sander)
 (global-set-key (kbd "C-c 5") 'paola)
 (global-set-key (kbd "C-c 8") 'ksbuild8)
-
-(when (eq system-type 'windows-nt)
-  (defun enzo () (interactive) (find-file "/plink:raliccia@10.26.15.66:/home/raliccia/"))
-  (global-set-key (kbd "C-c 0") 'enzo))
