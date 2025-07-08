f2387822fab41257100977376d201db8e8dd18ef
LICCIARDO Raphaele Salvatore <raphaele.licciardo@frequentis.com>
2025-07-08 09:04:58 +0200

    ssh: better default opened folders

--- _base/ssh.el
+++ _base/ssh.el
@@ -9,61 +9,61 @@
 (defun enzo () (interactive)
   (progn
   (dired "/plink:raliccia@10.26.15.66:/home/raliccia/")
-  (dired "/plink:raliccia@10.26.15.66:/home/raliccia/dev/fco/develop/ccms")))
+  (dired "/plink:raliccia@10.26.15.66:/home/raliccia/dev/fco/ccms")))
 
 (defun ksbuild8 ()
   (interactive)
   (progn
     (dired "/plink:raliccia@ksbuild8:/home_fco/raliccia/")
     (dired "/plink:raliccia@ksbuild8:/home_fco/raliccia/paths.txt")
-    (dired "/plink:raliccia@ksbuild8:/home1/products/ccms/kickstarts/raliccia/")
     (dired "/plink:raliccia@ksbuild8:/home1/tmp/raliccia/")
-    (dired "/plink:raliccia@ksbuild8:/home1/kickstart_iso/nis/ccms/rlicciar/")))
+    (dired "/plink:raliccia@ksbuild8:/home1/kickstart_iso/nis/ccms/rlicciar/")
+	(dired "/plink:raliccia@ksbuild8:/home1/products/ccms/kickstarts/raliccia/")))
 
 (defun nikos ()
   (interactive)
   (progn
-    (dired "/plink:root@testvm1:/")
     (dired "/plink:root@testvm1:/var/log/ccms/")
     (dired "/plink:root@testvm1:/usr/share/ccms/ansible/")
     (dired "/plink:root@testvm1:/usr/lib/python3.12/site-packages/ccms/")
-    (dired "/plink:root@testvm1:/etc/ccms/")))
+    (dired "/plink:root@testvm1:/etc/ccms/")
+    (dired "/plink:root@testvm1:/")))
 
 (defun zeudi ()
   (interactive)
   (progn
-    (dired "/plink:root@testvm2:/")
     (dired "/plink:root@testvm2:/var/log/ccms/")
     (dired "/plink:root@testvm2:/usr/share/ccms/ansible/")
     (dired "/plink:root@testvm2:/usr/lib/python3.12/site-packages/ccms/")
-    (dired "/plink:root@testvm2:/etc/ccms/")))
+    (dired "/plink:root@testvm2:/etc/ccms/")
+    (dired "/plink:root@testvm2:/")))
 
 (defun giulia ()
   (interactive)
   (progn
-    (dired "/plink:root@testvm3:/")
     (dired "/plink:root@testvm3:/var/log/ccms/")
     (dired "/plink:root@testvm3:/usr/share/ccms/ansible/")
     (dired "/plink:root@testvm3:/usr/lib/python3.12/site-packages/ccms/")
-    (dired "/plink:root@testvm3:/etc/ccms/")))
+    (dired "/plink:root@testvm3:/etc/ccms/")
+    (dired "/plink:root@testvm3:/")))
 
 (defun sander ()
   (interactive)
   (progn
-    (dired "/plink:root@testvm4:/")
     (dired "/plink:root@testvm4:/var/log/ccms/")
     (dired "/plink:root@testvm4:/usr/share/ccms/ansible/")
     (dired "/plink:root@testvm4:/usr/lib/python3.12/site-packages/ccms/")
-    (dired "/plink:root@testvm4:/etc/ccms/")))
+    (dired "/plink:root@testvm4:/etc/ccms/")
+    (dired "/plink:root@testvm4:/")))
 
 (defun paola ()
   (interactive)
   (progn
-    (dired "/plink:root@testvm5:/")
-    (dired "/plink:root@testvm5:/var/log/ccms/")
+    (dired "/plink:root@testvm5:/var/log/ccms/")p
     (dired "/plink:root@testvm5:/usr/share/ccms/ansible/")
     (dired "/plink:root@testvm5:/usr/lib/python3.12/site-packages/ccms/")
-    (dired "/plink:root@testvm5:/etc/ccms/")))
+    (dired "/plink:root@testvm5:/etc/ccms/")
+    (dired "/plink:root@testvm5:/")))
 
 (global-set-key (kbd "C-c 0") 'enzo)
 (global-set-key (kbd "C-c 1") 'nikos)
