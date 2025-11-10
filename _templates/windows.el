a07eee9329155e056893e47966da45a8b156aad1
LICCIARDO Raphaele Salvatore <raphaele.licciardo@frequentis.com>
2025-11-10 10:16:15 +0100

    Update Windows

--- _templates/windows.el
+++ _templates/windows.el
@@ -1,7 +1,10 @@
 (load "~/.emacs.d/_base/ssh.el")
 
 ;; Theme
-(lr/legacy)
+;; (lr/legacy)
+(lr/enable-custom-font-iosevka)
+(lr/load-theme 'modus-operandi)
+(global-whitespace-mode 0)
 
 (custom-set-faces
  '(mode-line ((t (:box nil))))
