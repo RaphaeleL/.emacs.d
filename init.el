33cb2559410bea859052791083578dc47dc2019b
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2025-12-03 08:48:49 +0100

    comment plugin manager in init.el

--- init.el
+++ init.el
@@ -1,11 +1,12 @@
 ;; === PLUGIN MANAGER ==============================
-(require 'package)
-(setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
-(setq package-archives
-      '(("melpa" . "https://melpa.org/packages/")
-        ("gnu"   . "https://elpa.gnu.org/packages/")))
-(package-initialize)
-(unless package-archive-contents (package-refresh-contents))
+(require 'package)                                     ;; load Emacs' built-in package manager
+(setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3") ;; workaround for TLS1.3 endpoint issues
+(setq package-archives                                 ;; define packages location
+      '(("melpa" . "https://melpa.org/packages/")      ;; -> MELPA for community packages
+        ("gnu"   . "https://elpa.gnu.org/packages/"))) ;; -> GNU ELPA for official GNU packages
+(package-initialize)                                   ;; init pkg system and activate installed pkgs
+(unless package-archive-contents                       ;; if package list is empty
+  (package-refresh-contents))                          ;; -> fetch the latest pkg index from archives
 
 ;; === BASIC AND DEFAULT CONFIGURATION =============
 (load "~/.emacs.d/_base/functions.el" 'noerror 'nomessage)  ;; a collection of useful functions
