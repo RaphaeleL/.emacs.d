c32696a4dbcd3fdae1715323d63cfdf2b652352a
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2025-12-04 05:33:43 +0100

    no more paths

--- _templates/linux.el
+++ _templates/linux.el
@@ -1,18 +1,5 @@
 (load "~/.emacs.d/_base/lsp.el")
 
-;; Theme
 (lr/enable-custom-font-iosevka)
 (lr/theme 'gruberdarker)
 (global-whitespace-mode 1)
-
-;; Extend exec-path and PATH environment variable
-(let ((paths '("~/bin"
-               "~/.local/bin"
-               "/usr/local/bin"
-               "~/go/bin"
-               "~/.cargo/bin")))
-
-  (dolist (p (reverse paths))
-    (when (file-directory-p (expand-file-name p))
-      (add-to-list 'exec-path (expand-file-name p))
-      (setenv "PATH" (concat (expand-file-name p) path-separator (getenv "PATH"))))))
