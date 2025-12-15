24a431a614845afe030e7d7f23f41defb770659e
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2025-12-15 13:16:17 +0100

    fairfax font

--- _base/functions.el
+++ _base/functions.el
@@ -73,7 +73,7 @@
 
 (defun lr/enable-custom-font-legacy ()
   (interactive)
-  (let ((fonts '("Consoleet Darwin" "Consoleet Darwin Smooth"))
+  (let ((fonts '("Fairfax" "Consoleet Darwin" "Consoleet Darwin Smooth"))
         (size 18)
         chosen-font)
     (dolist (f fonts)
