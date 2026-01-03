f756782fd4e79b5eea0ce8b2e4c8aa56d3d8a00c
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2026-01-03 16:28:08 +0100

    output.el

--- output.el
+++ output.el
@@ -4,7 +4,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages nil))
+ '(package-selected-packages
+   '(company consult diredfl dockerfile-mode go-mode jenkinsfile-mode
+             jinja2-mode magit marginalia markdown-mode mason
+             modus-themes mood-line move-text multiple-cursors
+             orderless paredit rpm-spec-mode rust-mode simpleclip
+             vertico web-mode yaml-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
